5fc3c320cc20bda43d234c0e478e6a1fdaa41066
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2018-08-29 23:33:17 +0100

    add 2017

---
+++ 2017/js/scripts.js
@@ -0,0 +1,201 @@
+$(document).ready(function() {
+    "use strict";
+
+    // Smooth scroll to inner links
+        var innerLinks = $('a.inner-link');
+
+        if(innerLinks.length){
+            innerLinks.each(function(){
+                var link = $(this);
+                var href = link.attr('href');
+                if(href.charAt(0) !== "#"){
+                    link.removeClass('inner-link');
+                }
+            });
+
+            var offset = 0;
+            if($('body[data-smooth-scroll-offset]').length){
+                offset = $('body').attr('data-smooth-scroll-offset');
+                offset = offset*1;
+            }
+
+            smoothScroll.init({
+                selector: '.inner-link',
+                selectorHeader: null,
+                speed: 750,
+                easing: 'easeInOutCubic',
+                offset: offset
+            });
+        }
+
+    // Append .background-image-holder <img>'s as CSS backgrounds
+
+    $('.background-image-holder').each(function() {
+        var imgSrc = $(this).children('img').attr('src');
+        $(this).css('background', 'url("' + imgSrc + '")');
+        $(this).children('img').hide();
+        $(this).css('background-position', 'initial');
+    });
+
+    // Fade in background images
+
+    setTimeout(function() {
+        $('.background-image-holder').each(function() {
+            $(this).addClass('fadeIn');
+        });
+    }, 200);
+
+    // Icon bulleted lists
+
+    $('ul[data-bullet]').each(function(){
+        var bullet = $(this).attr('data-bullet');
+        $(this).find('li').prepend('<i class="'+bullet+'"></i>');
+    });
+
+    // Multipurpose Modals
+
+    jQuery('.foundry_modal[modal-link]').remove();
+
+    if($('.foundry_modal').length && (!jQuery('.modal-screen').length)){
+        // Add a div.modal-screen if there isn't already one there.
+        var modalScreen = jQuery('<div />').addClass('modal-screen').appendTo('body');
+
+    }
+
+    jQuery('.foundry_modal').click(function(){
+        jQuery(this).addClass('modal-acknowledged');
+    });
+
+    jQuery(document).on('wheel mousewheel scroll', '.foundry_modal, .modal-screen', function(evt){
+        $(this).get(0).scrollTop += (evt.originalEvent.deltaY);
+        return false;
+    });
+
+    $('.modal-container:not([modal-link])').each(function(index) {
+        if(jQuery(this).find('iframe[src]').length){
+        	jQuery(this).find('.foundry_modal').addClass('iframe-modal');
+        	var iframe = jQuery(this).find('iframe');
+        	iframe.attr('data-src',iframe.attr('src'));
+            iframe.attr('src', '');
+
+        }
+        jQuery(this).find('.btn-modal').attr('modal-link', index);
+
+        // Only clone and append to body if there isn't already one there
+        if(!jQuery('.foundry_modal[modal-link="'+index+'"]').length){
+            jQuery(this).find('.foundry_modal').clone().appendTo('body').attr('modal-link', index).prepend(jQuery('<i class="ti-close close-modal">'));
+        }
+    });
+
+    // Autoshow modals
+
+	$('.foundry_modal[data-time-delay]').each(function(){
+		var modal = $(this);
+		var delay = modal.attr('data-time-delay');
+		modal.prepend($('<i class="ti-close close-modal">'));
+    	if(typeof modal.attr('data-cookie') != "undefined"){
+        	if(!mr_cookies.hasItem(modal.attr('data-cookie'))){
+                setTimeout(function(){
+        			modal.addClass('reveal-modal');
+        			$('.modal-screen').addClass('reveal-modal');
+        		},delay);
+            }
+        }else{
+            setTimeout(function(){
+                modal.addClass('reveal-modal');
+                $('.modal-screen').addClass('reveal-modal');
+            },delay);
+        }
+	});
+
+
+  jQuery('.close-modal').unbind('click').click(function(){
+    var modal = jQuery(this).closest('.foundry_modal');
+      modal.toggleClass('reveal-modal');
+      if(typeof modal.attr('data-cookie') !== "undefined"){
+          mr_cookies.setItem(modal.attr('data-cookie'), "true", Infinity);
+      }
+    if(modal.find('iframe').length){
+          modal.find('iframe').attr('src', '');
+      }
+      jQuery('.modal-screen').removeClass('reveal-modal');
+  });
+
+    // Tabbed Content
+
+    $('.tabbed-content').each(function() {
+        $(this).append('<ul class="content"></ul>');
+    });
+
+    $('.tabs li').each(function() {
+        var originalTab = $(this),
+            activeClass = "";
+        if (originalTab.is('.tabs>li:first-child')) {
+            activeClass = ' class="active"';
+        }
+        var tabContent = originalTab.find('.tab-content').detach().wrap('<li' + activeClass + '></li>').parent();
+        originalTab.closest('.tabbed-content').find('.content').append(tabContent);
+    });
+
+    $('.tabs li').click(function() {
+        $(this).closest('.tabs').find('li').removeClass('active');
+        $(this).addClass('active');
+        var liIndex = $(this).index() + 1;
+        $(this).closest('.tabbed-content').find('.content>li').removeClass('active');
+        $(this).closest('.tabbed-content').find('.content>li:nth-of-type(' + liIndex + ')').addClass('active');
+    });
+
+    $('.logo-carousel').flexslider({
+        minItems: 1,
+        maxItems: 4,
+        move: 1,
+        itemWidth: 200,
+        itemMargin: 0,
+        animation: "slide",
+        slideshow: true,
+        slideshowSpeed: 3000,
+        directionNav: false,
+        controlNav: false
+    });
+
+});
+
+var mr_cookies = {
+  getItem: function (sKey) {
+    if (!sKey) { return null; }
+    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+  },
+  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+    var sExpires = "";
+    if (vEnd) {
+      switch (vEnd.constructor) {
+        case Number:
+          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+          break;
+        case String:
+          sExpires = "; expires=" + vEnd;
+          break;
+        case Date:
+          sExpires = "; expires=" + vEnd.toUTCString();
+          break;
+      }
+    }
+    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    return true;
+  },
+  removeItem: function (sKey, sPath, sDomain) {
+    if (!this.hasItem(sKey)) { return false; }
+    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+    return true;
+  },
+  hasItem: function (sKey) {
+    if (!sKey) { return false; }
+    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+  },
+  keys: function () {
+    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+    for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+    return aKeys;
+  }
+};
